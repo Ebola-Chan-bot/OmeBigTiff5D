390d06e9d7189dff6cd5e08a961a233d8f59706d
埃博拉酱 <vhtmfu@outlook.com>
2021-12-31 14:26:45 +0800

    OmeBigTiff5D增加只读打开功能

--- MATLAB工具箱/+OBT5/CreationDisposition.m
+++ MATLAB工具箱/+OBT5/CreationDisposition.m
@@ -1,11 +1,13 @@
 classdef CreationDisposition<uint8
 	%创建OmeBigTiff5D对象时，如果指定的文件存在或不存在，应该执行何种操作？
 	enumeration
+		%以只读模式打开
+		OpenRead(0)
 		%打开现存文件
-		OpenExisting(0)
+		OpenExisting(1)
 		%文件可能不存在，即使存在也可能有格式错误。如果无法打开，请创建新文件。
-		OpenOrCreate(1)
+		OpenOrCreate(2)
 		%无论文件是否存在，直接覆盖创建新文件。
-		Overwrite(2)
+		Overwrite(3)
 	end
 end
--- MATLAB工具箱/+OBT5/Version.m
+++ MATLAB工具箱/+OBT5/Version.m
@@ -1,3 +1,3 @@
 function V=Version
-V.Me='3.0.1';
+V.Me='3.1.0';
 V.Matlab='R2021b';
